c4a3ec3597cef895cfe03b089922a37676a7ad02
gal <gallevy1994@gmail.com>
2024-09-23 21:54:18 +0300

    with token fixed

--- src/main/java/com/handson/trip_planner/jwt/JwtAuthenticationController.java
+++ src/main/java/com/handson/trip_planner/jwt/JwtAuthenticationController.java
@@ -1,12 +1,14 @@
 package com.handson.trip_planner.jwt;
 
 import com.handson.trip_planner.model.Trip;
+import com.handson.trip_planner.security.TokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -31,22 +33,25 @@ public class JwtAuthenticationController {
     private DBUserService userService;
 
     @Autowired
+    private TokenProvider tokenProvider;
+
+    @Autowired
     private PasswordEncoder passwordEncoder;
     @Autowired
     AuthenticationManager am;
 
     @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
     public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
-        System.out.println("*******Received login request for user: " + authenticationRequest.getUsername());
-        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
-        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
-        final String token = jwtTokenUtil.generateToken(userDetails);
-        System.out.println("*******Token: " + token);
-
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authenticationRequest.getUsername(),
+                        authenticationRequest.getPassword()
+                )
+        );
+        final String token = tokenProvider.createToken(authentication);
         Long userId = userService.findUserName(authenticationRequest.getUsername())
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"))
                 .getId();
-
         return ResponseEntity.ok(new JwtResponse(token, userId));
     }
 
